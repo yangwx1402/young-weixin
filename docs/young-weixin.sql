db97ca93e3857caafd664d33f9424101eb894ede
yangyong <398901650@qq.com>
2016-02-23 22:57:01 +0800

    关注用户表脚本

---
+++ docs/young-weixin.sql
@@ -0,0 +1,35 @@
+﻿/*
+SQLyog Community Edition- MySQL GUI v6.05
+Host - 5.6.26 : Database - young-weixin
+*********************************************************************
+Server version : 5.6.26
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+create database if not exists `young-weixin`;
+
+USE `young-weixin`;
+
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+/*Table structure for table `weixin_subscribe_info` */
+
+DROP TABLE IF EXISTS `weixin_subscribe_info`;
+
+CREATE TABLE `weixin_subscribe_info` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) DEFAULT NULL,
+  `status` varchar(20) DEFAULT NULL,
+  `createtime` varchar(20) DEFAULT NULL,
+  `updatetime` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `weixin_subscribe_info` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
