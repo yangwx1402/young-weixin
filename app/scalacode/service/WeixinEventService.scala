c951eb7aa03556144762001de1b850e12ce6ab6d
yangyong <398901650@qq.com>
2016-03-02 22:17:54 +0800

    修改了关注回复bug

--- app/scalacode/service/WeixinEventService.scala
+++ app/scalacode/service/WeixinEventService.scala
@@ -5,13 +5,12 @@ import javacode.util.xml.XStreamFactory
 import play.Logger
 
 import scalacode.dao.EventProcessDao
-import scalacode.entity.{ImageContentResponseMessage, ImageContentResponseMessageArticleItem, SubscribeEvent}
+import scalacode.entity.{WeixinConstants, ImageContentResponseMessage, ImageContentResponseMessageArticleItem, SubscribeEvent}
 
 /**
  * Created by Administrator on 2016/2/24.
  */
 class WeixinEventService {
-
   /**
    * 处理用户关注和取消关注事件
    * @param subscribeEvent
@@ -24,11 +23,15 @@ class WeixinEventService {
     } else {
       EventProcessDao.saveUserSubscribe(subscribeEvent)
     }
-    val stream = XStreamFactory.getInstance(Array(classOf[ImageContentResponseMessage],classOf[ImageContentResponseMessageArticleItem]))
-    val responseMessage = stream.fromXmlStream(this.getClass.getResourceAsStream("/subscribe-response.xml"),classOf[ImageContentResponseMessage])
-    responseMessage.FromUserName = subscribeEvent.ToUserName
-    responseMessage.ToUserName = subscribeEvent.FromUserName
-    responseMessage.CreateTime = System.currentTimeMillis()+""
-    stream.toXml(responseMessage)
+    if (WeixinConstants.MSG_TYPE_EVENT_SUBSCRIBE.equals(subscribeEvent.Event)) {
+      val stream = XStreamFactory.getInstance(Array(classOf[ImageContentResponseMessage], classOf[ImageContentResponseMessageArticleItem]))
+      val responseMessage = stream.fromXmlStream(this.getClass.getResourceAsStream("/subscribe-response.xml"), classOf[ImageContentResponseMessage])
+      responseMessage.FromUserName = subscribeEvent.ToUserName
+      responseMessage.ToUserName = subscribeEvent.FromUserName
+      responseMessage.CreateTime = System.currentTimeMillis() + ""
+      stream.toXml(responseMessage)
+    } else {
+      WeixinConstants.MSG_NO_CONTENT
+    }
   }
 }
